245b682cb96371f09a4554d9ff470019b74d55d7
MacB71 <macb71@gmail.com>
2023-12-11 18:23:56 +0100

    fix: add localhost to crs

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions = {
-    origin: 'https://angular17-app.vercel.app/',
+    origin: ['https://angular17-app.vercel.app/', 'http://localhost:4200'],
   };
   app.enableCors(corsOptions);
   await app.listen(3000);
