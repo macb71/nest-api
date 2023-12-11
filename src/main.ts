4063385610892e4f7f091a7f60d2629f85512001
MacB71 <macb71@gmail.com>
2023-12-11 18:21:50 +0100

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions = {
+    origin: 'https://angular17-app.vercel.app/',
+  };
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
