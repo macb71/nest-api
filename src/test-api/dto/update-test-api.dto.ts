478521dd34ccb6b0fa5113600ce8f1d137caf866
MacB71 <macb71@gmail.com>
2023-12-13 17:55:54 +0100

    chore: delete test dirs

--- src/test-api/dto/update-test-api.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTestApiDto } from './create-test-api.dto';
-
-export class UpdateTestApiDto extends PartialType(CreateTestApiDto) {}
--- src/test-api/test-api.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TestApiService } from './test-api.service';
-import { TestApiController } from './test-api.controller';
-
-@Module({
-  controllers: [TestApiController],
-  providers: [TestApiService],
-})
-export class TestApiModule {}
