c3fb11cbe53b4661d5aad58c2fe50144375f6756
MacB71 <macb71@gmail.com>
2023-12-13 18:37:04 +0100

    feat: init and add auth #Authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TestModule } from './test/test.module';
-import { TestApiModule } from './test-api/test-api.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TestModule, TestApiModule],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
