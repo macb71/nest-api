a5a812b88baf79afaf892d0894cbd229b37f9e06
MacB71 <macb71@gmail.com>
2023-12-13 16:55:08 +0100

    fix: fix origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions = {
-    origin: ['https://angular17-app.vercel.app/', 'http://localhost:4200'],
+    origin: ['https://angular17-app.vercel.app/'],
   };
   app.enableCors(corsOptions);
   await app.listen(3000);
