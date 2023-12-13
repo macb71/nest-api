072b5aafeeaf3f3400f2742a4a4179a52f9f1beb
MacB71 <macb71@gmail.com>
2023-12-13 17:44:04 +0100

    feat: cors working

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions = {
-    origin: ['https://angular17-app.vercel.app/'],
+    origin: ['http://localhost:4200'],
   };
   app.enableCors(corsOptions);
   await app.listen(3000);
